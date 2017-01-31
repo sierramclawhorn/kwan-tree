1d1041ab0058e10c649bdf075753468e4926fbb4
sierramclawhorn <sierramclawhorn@gmail.com>
2017-01-31 10:54:02 -0800

    Require dotenv; Add fb developer keys

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,7 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, 'YOUR-APP-ID-HERE', 'YOUR-APP-SECRET-HERE'
+  provider :facebook, '640470299487147', FB_LOGIN_SECRET
 end
+
+
