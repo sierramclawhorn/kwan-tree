fd2a28d35cedf4c00ad68db9fae929c5836eee92
sierramclawhorn <sierramclawhorn@gmail.com>
2017-02-09 22:29:05 -0800

    fix findplaytime promise

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -6,45 +6,48 @@ $(document).ready(function() {
 	var replacementVideo = "https://player.vimeo.com/video/199619376";
 	var playTime = "";
 
-	var findPlayTime = function() {
-		player.getCurrentTime().then(function(seconds) {
-		    playTime = seconds;
-		    console.log(playTime);
-		}).catch(function(error) {
-		    // an error occurred
-		});
-	};
+		var findPlayTime = function() {
+			player.getCurrentTime().then(function(seconds) {
+			    playTime = seconds;
+			    console.log("in findplaytime funct " + playTime);
+			    updateLink();
+			}).catch(function(error) {
+			    // an error occurred
+			});
+		};
 
-	var videoSwitchListener = function() {
-		$('#btnClick').click(function() {
+		var buttonListener = function() {
+			console.log("before findplaytime");
 			findPlayTime();
+		};
+
+		var updateLink = function(){
 			$('#video iframe').attr('src', replacementVideo);
+			startPlayTime();
+			console.log('after findPlayTime');
+			console.log('in switcher ' + playTime);
 
-			console.log(playTime);
-			console.log("playing");
-		});
-	};
+		};
 
-	var startPlayTime = function() {
-		player.setCurrentTime(playTime).then(function(seconds) {
-    		// seconds = the actual time that the player seeked to
-		}).catch(function(error) {
-    		switch (error.name) {
-        		case 'RangeError':
-            		// the time was less than 0 or greater than the video’s duration
-            		break;
-        		default:
-            		// some other error occurred
-            		break;
-    		}
-		});
-	};
-
-
-
-  	videoSwitchListener(function() {
-    	startPlayTime(function() {
-    	});
+		var startPlayTime = function() {
+			player.setCurrentTime(playTime).then(function(seconds) {
+	    		// seconds = the actual time that the player seeked to
+	    		console.log('in new video playTime ' + playTime);
+			}).catch(function(error) {
+	    		switch (error.name) {
+	        		case 'RangeError':
+	            		// the time was less than 0 or greater than the video’s duration
+	            		break;
+	        		default:
+	            		// some other error occurred
+	            		break;
+	    		}
+			});
+		};
+	
+	$('#btnClick').click(function() {
+		console.log('clicked!');
+		buttonListener();
 	});
 
 });
