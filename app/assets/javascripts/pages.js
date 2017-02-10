9f9097fc3920f43057e12970740fa82253b9150c
sierramclawhorn <sierramclawhorn@gmail.com>
2017-02-09 18:21:51 -0800

    find current play time

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -1,42 +1,50 @@
-$(document).ready(function () {
+$(document).ready(function() {
 
 	var iframe = document.querySelector('iframe');
     var player = new Vimeo.Player(iframe);
-
-    // player.on('play', function() {
-    //     console.log('played the video!');
-    // });
-    // player.getVideoTitle().then(function(title) {
-    //     console.log('title:', title);
-    // });
-
-
 	var initialVideo = "https://player.vimeo.com/video/199619694";
 	var replacementVideo = "https://player.vimeo.com/video/199619376";
 	var playTime = "";
-	
-	$('#btnClick').on('click', function() {
+
+	var findPlayTime = function() {
 		player.getCurrentTime().then(function(seconds) {
-		    // seconds = the current playback position
-		    playTime = seconds
+		    playTime = seconds;
+		    console.log(playTime);
 		}).catch(function(error) {
 		    // an error occurred
-		});		
+		});
+	};
 
-		// changes the video
-		$('#video iframe').attr('src', replacementVideo);
-		
-		player.setCurrentTime(30.456).then(function(seconds) {
-		    // seconds = the actual time that the player seeked to
+	var videoSwitchListener = function() {
+		$('#btnClick').click(function() {
+			findPlayTime();
+			$('#video iframe').attr('src', replacementVideo);
+
+			console.log(playTime);
+			console.log("playing");
+		});
+	};
+
+	var startPlayTime = function() {
+		player.setCurrentTime(playTime).then(function(seconds) {
+    		// seconds = the actual time that the player seeked to
 		}).catch(function(error) {
-		    switch (error.name) {
-		        case 'RangeError':
-		            // the time was less than 0 or greater than the video’s duration
-		            break;
-		        default:
-		            // some other error occurred
-		            break;
-		    }
-		});		
+    		switch (error.name) {
+        		case 'RangeError':
+            		// the time was less than 0 or greater than the video’s duration
+            		break;
+        		default:
+            		// some other error occurred
+            		break;
+    		}
+		});
+	};
+
+
+
+  	videoSwitchListener(function() {
+    	startPlayTime(function() {
+    	});
 	});
-})
+
+});
