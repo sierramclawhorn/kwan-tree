0fb9493a19b4bf96a5412a7473ea17c3dca1bf9c
sierramclawhorn <sierramclawhorn@gmail.com>
2017-02-12 18:57:57 -0800

    Add button to switch back

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -2,82 +2,91 @@ $(document).ready(function() {
 
 	var iframe = $('#video');
     var player = new Vimeo.Player(iframe);
+    var initialVideo = '199619694';
     var replacementVideo = '199619376';
-    var playTime = "";
+    var playTime = '';
 
-    $('#btnClick').click(function() {
-    	player.getCurrentTime().then(function(seconds) {
+
+    $('#initialBtn').click(function() {
+  		player.getCurrentTime().then(function(seconds) {
 			playTime = seconds;
 			console.log('grab time ' + playTime);
+		}).catch(function(error) {
+			console.log('Error: ${error}');
 		});
 
 		player.loadVideo(replacementVideo).then(function(id) {
 			setNewVideo();
 			console.log('switch video');
+		}).catch(function(error) {
+		    switch (error.name) {
+		        case 'TypeError':
+		            break;
+		        case 'PasswordError':
+		            break;
+		        case 'PrivacyError':
+		            break;
+		        default:
+		            break;
+		    };
+		    console.log(error.name + '${error}');
 		});
 
 		var setNewVideo = function() {
 			player.setCurrentTime(playTime).then(function(seconds) {
 				console.log('new video set to ' + seconds);
+			}).catch(function(error) {
+    			switch (error.name) {
+    			    case 'RangeError':
+    			        break;
+    			    default:
+    			        break;
+    			};
+    			console.log(error.name + '${error}');
 			});
 		};
+
+		$('.btnClick').toggle();
+	});
+
+
+	$('#replacementBtn').click(function() {
+  		player.getCurrentTime().then(function(seconds) {
+			playTime = seconds;
+			console.log('grab time ' + playTime);
+		}).catch(function(error) {
+		});
+
+		player.loadVideo(initialVideo).then(function(id) {
+			setNewVideo();
+			console.log('switch video');
+		}).catch(function(error) {
+		    switch (error.name) {
+		        case 'TypeError':
+		            break;
+		        case 'PasswordError':
+		            break;
+		        case 'PrivacyError':
+		            break;
+		        default:
+		            break;
+		    };
+		});
+
+		var setNewVideo = function() {
+			player.setCurrentTime(playTime).then(function(seconds) {
+				console.log('new video set to ' + seconds);
+			}).catch(function(error) {
+    			switch (error.name) {
+    			    case 'RangeError':
+    			        break;
+    			    default:
+    			        break;
+    			};
+			});
+		};
+
+		$('.btnClick').toggle();
 	});
 
 })
-
-
-
-
-// $(document).ready(function() {
-	
-// 	var iframe = $('#video');
-//     var player = new Vimeo.Player(iframe);
-// 	var initialVideo = "https://player.vimeo.com/video/199619694";
-// 	var replacementVideo = "https://player.vimeo.com/video/199619376";
-// 	var playTime = "";
-
-
-// 		var findPlayTime = function() {
-// 			player.getCurrentTime().then(function(seconds) {
-// 			    playTime = seconds;
-// 			    console.log("in findplaytime funct " + playTime);
-// 			    updateLink();
-// 			}).catch(function(error) {
-// 			    // an error occurred
-// 			});
-// 		};
-
-// 		var buttonListener = function() {
-// 			console.log("before findplaytime");
-// 			findPlayTime();
-// 		};
-
-// 		var updateLink = function(){
-// 			$('#videoDiv #video').attr('src', replacementVideo);
-// 			startPlayTime();
-// 			console.log('after findPlayTime');
-// 			console.log('in switcher ' + playTime);
-// 		};
-
-// 		var startPlayTime = function() {
-// 			player.setCurrentTime(playTime).then(function(seconds) {
-// 	    		// seconds = the actual time that the player seeked to
-// 	    		console.log(iframe.attr('src'));
-// 	    		console.log('in new video playTime ' + seconds);
-// 			}).catch(function(error) {
-// 	    		switch (error.name) {
-// 	        		case 'RangeError':
-// 	            		// the time was less than 0 or greater than the video’s duration
-// 	            		break;
-// 	        		default:
-// 	            		// some other error occurred
-// 	            		break;
-// 	    		}
-// 			});
-// 		};
-
-// 	$('#btnClick').click(function() {
-// 		buttonListener();
-// 	});
-
-// });
