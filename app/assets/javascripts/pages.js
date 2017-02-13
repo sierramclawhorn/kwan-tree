fa896528de7fa64f44b005841ab809373926b334
sierramclawhorn <sierramclawhorn@gmail.com>
2017-02-12 21:46:13 -0800

    Add additional comments

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -1,44 +1,44 @@
 $(document).ready(function() {
 
 	var iframe = $('#video');
-    var player = new Vimeo.Player(iframe);
+	var player = new Vimeo.Player(iframe);
     var initialVideo = '199619694'; //change to digits in initial video's link (https://player.vimeo.com/video/199619694)
     var replacementVideo = '199619376'; //change to digits in replacement video's link
     var playTime = '';
 
-	var findPlaybackTime = function () {
-  		player.getCurrentTime().then(function(seconds) {
-			playTime = seconds;
-		}).catch(function(error) {
-		});
-	};
+    var findPlaybackTime = function () {
+    	player.getCurrentTime().then(function(seconds) {
+    		playTime = seconds;
+    	}).catch(function(error) {
+    	});
+    };
 
-	var setNewVideo = function() {
-		player.setCurrentTime(playTime).then(function(seconds) {
-		}).catch(function(error) {
-		});
-	};
+    var setNewVideo = function() {
+    	player.setCurrentTime(playTime).then(function(seconds) {
+    	}).catch(function(error) {
+    	});
+    };
 
     $('#initialBtn').click(function() {
     	findPlaybackTime();
 
     	player.loadVideo(replacementVideo).then(function(id) {
-			setNewVideo();
-		}).catch(function(error) {
-		});
+    		setNewVideo();
+    	}).catch(function(error) {
+    	});
 
-		$('.btnClick').toggle();
-	});
+    	$('.btnClick').toggle();
+    });
 
-	$('#replacementBtn').click(function() {
-		findPlaybackTime();
+    $('#replacementBtn').click(function() {
+    	findPlaybackTime();
 
     	player.loadVideo(initialVideo).then(function(id) {
-			setNewVideo();
-		}).catch(function(error) {
-		});
+    		setNewVideo();
+    	}).catch(function(error) {
+    	});
 
-		$('.btnClick').toggle();
-	});
+    	$('.btnClick').toggle();
+    });
 
 });
