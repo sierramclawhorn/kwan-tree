7ac71a1ce2303c6a62e811c100588cb380988b7c
sierramclawhorn <sierramclawhorn@gmail.com>
2017-02-12 19:19:29 -0800

    Add instruction comments

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -2,91 +2,43 @@ $(document).ready(function() {
 
 	var iframe = $('#video');
     var player = new Vimeo.Player(iframe);
-    var initialVideo = '199619694';
-    var replacementVideo = '199619376';
+    var initialVideo = '199619694'; //change to digits in initial video's link (https://player.vimeo.com/video/199619694)
+    var replacementVideo = '199619376'; //change to digits in replacement video's link
     var playTime = '';
 
+	var findPlaybackTime = function () {
+  		player.getCurrentTime().then(function(seconds) {
+			playTime = seconds;
+		}).catch(function(error) {
+		});
+	};
+
+	var setNewVideo = function() {
+		player.setCurrentTime(playTime).then(function(seconds) {
+		}).catch(function(error) {
+		});
+	};
 
     $('#initialBtn').click(function() {
-  		player.getCurrentTime().then(function(seconds) {
-			playTime = seconds;
-			console.log('grab time ' + playTime);
-		}).catch(function(error) {
-			console.log('Error: ${error}');
-		});
+    	findPlaybackTime();
 
-		player.loadVideo(replacementVideo).then(function(id) {
+    	player.loadVideo(replacementVideo).then(function(id) {
 			setNewVideo();
-			console.log('switch video');
 		}).catch(function(error) {
-		    switch (error.name) {
-		        case 'TypeError':
-		            break;
-		        case 'PasswordError':
-		            break;
-		        case 'PrivacyError':
-		            break;
-		        default:
-		            break;
-		    };
-		    console.log(error.name + '${error}');
 		});
-
-		var setNewVideo = function() {
-			player.setCurrentTime(playTime).then(function(seconds) {
-				console.log('new video set to ' + seconds);
-			}).catch(function(error) {
-    			switch (error.name) {
-    			    case 'RangeError':
-    			        break;
-    			    default:
-    			        break;
-    			};
-    			console.log(error.name + '${error}');
-			});
-		};
 
 		$('.btnClick').toggle();
 	});
-
 
 	$('#replacementBtn').click(function() {
-  		player.getCurrentTime().then(function(seconds) {
-			playTime = seconds;
-			console.log('grab time ' + playTime);
-		}).catch(function(error) {
-		});
+		findPlaybackTime();
 
-		player.loadVideo(initialVideo).then(function(id) {
+    	player.loadVideo(initialVideo).then(function(id) {
 			setNewVideo();
-			console.log('switch video');
 		}).catch(function(error) {
-		    switch (error.name) {
-		        case 'TypeError':
-		            break;
-		        case 'PasswordError':
-		            break;
-		        case 'PrivacyError':
-		            break;
-		        default:
-		            break;
-		    };
 		});
-
-		var setNewVideo = function() {
-			player.setCurrentTime(playTime).then(function(seconds) {
-				console.log('new video set to ' + seconds);
-			}).catch(function(error) {
-    			switch (error.name) {
-    			    case 'RangeError':
-    			        break;
-    			    default:
-    			        break;
-    			};
-			});
-		};
 
 		$('.btnClick').toggle();
 	});
 
-})
+});
