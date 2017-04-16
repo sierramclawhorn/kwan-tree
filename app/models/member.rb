c1d1e7054735a792dc3945f11b139c44836d3808
sierramclawhorn <sierramclawhorn@gmail.com>
2017-04-15 23:14:44 -0400

    Remove format validation for birth/deathdate

--- app/models/member.rb
+++ app/models/member.rb
@@ -3,6 +3,6 @@ class Member < ApplicationRecord
 
 	validates_uniqueness_of :number
 	validates_presence_of :number
-	validates_format_of :birthdate, :with =>  /\A[0-9]{4}\Z/
-	validates_format_of :deathdate, :with =>  /\A[0-9]{4}\Z/
+	# validates_format_of :birthdate, :with =>  /\A[0-9]{4}\Z/
+	# validates_format_of :deathdate, :with =>  /\A[0-9]{4}\Z/
 end
