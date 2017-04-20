1996332c779ddc3dd4551154b417ee0367d2890e
sierramclawhorn <sierramclawhorn@gmail.com>
2017-04-20 12:06:20 -0700

    Add table structure for member display

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,10 @@
 class PagesController < ApplicationController
   def index
-  	@members = Member.all.order('number ASC')
+  	@members = Member.search(params[:term])
   end
+
+  def member_params
+  	params.require(:member).permit(:name, :number, :spouse, :location, :term)
+	end
+
 end
--- app/models/member.rb
+++ app/models/member.rb
@@ -6,4 +6,12 @@ class Member < ApplicationRecord
 	validates_presence_of :name
 	# validates_format_of :birthdate, :with =>  /\A[0-9]{4}\Z/
 	# validates_format_of :deathdate, :with =>  /\A[0-9]{4}\Z/
+
+	def self.search(term)
+ 		if term
+ 		  where('name LIKE ?', "%#{term}%").order('number ASC')
+ 		else
+ 		  all
+ 		end
+	end
 end
